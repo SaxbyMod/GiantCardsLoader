d71f66a6d096af1e63486d1732f44d460d2ad9e8
Thin Creator 3483 <102002463+SaxbyMod@users.noreply.github.com>
2024-06-03 17:18:38 -0400

    Style change

--- Plugin.cs
+++ Plugin.cs
@@ -1,4 +1,5 @@
-using BepInEx;
+﻿using BepInEx;
+using BepInEx.Configuration;
 using DiskCardGame;
 using HarmonyLib;
 using InscryptionAPI.Card;
@@ -20,12 +21,22 @@ namespace ExampleMod
         private const string PluginName = "BossCardsLoader";
         private const string PluginVersion = "1.0.0";
         private const string PluginPrefix = "BossCardsLoader";
+        private static ConfigEntry<string> ReplaceMoon;
+        private static ConfigEntry<string> ReplaceLemoncello;
 
         // --------------------------------------------------------------------------------------------------------------------------------------------------
 
         // This is where you would run all of your other methods.
-        private void Awake()
+        public void Awake()
         {
+            ReplaceMoon = Config.Bind<string>("BossCardsLoader.ReplaceCards",
+                        "Moon Replacement",
+                        "",
+                        "What card should replace the Moon?");
+            ReplaceLemoncello = Config.Bind<string>("BossCardsLoader.ReplaceCards",
+                        "Lemoncello Replacement",
+                        "",
+                        "What card should replace the Lemoncello?");
             AddMEGALADON();
             AddSUM();
             // Apply our harmony patches.
@@ -33,63 +44,114 @@ namespace ExampleMod
             int i = 0;
             static void AddMEGALADON()
             {
-                CardInfo Megaladon = CardManager.New(
-
-                    // Card ID Prefix
-                    modPrefix: "BossCardsLoader",
-
-                    // Card internal name.
-                    "!GIANTCARD_MEGALADON",
-                    // Card display name.
-                    "MEGALADON",
-                    // Attack.
-                    1,
-                    // Health.
-                    200,
-                    // Descryption.
-                    description: "Megaladon Bites Hard especially on Squirrels"
-                )
-                .SetCost(0, 0, 0, null)
-                .AddAbilities(Ability.AllStrike, Ability.SquirrelOrbit, Ability.RandomAbility, Ability.Reach)
-                .AddTraits(Trait.Giant)
-                .AddSpecialAbilities(SpecialTriggeredAbility.GiantCard)
-                .AddAppearances(CardAppearanceBehaviour.Appearance.GiantAnimatedPortrait)
-                ;
+                if (ReplaceLemoncello.BoxedValue.Equals("BossCardsLoader_MEGALADON") && ReplaceMoon.BoxedValue.Equals("BossCardsLoader_MEGALADON"))
+                {
+                    CardInfo Megaladon = CardManager.BaseGameCards.CardByName("!GIANTCARD_MOON")
+                    .SetBasic(displayedName: "MEGALADON", attack: 2, health: 40, description: "Megaladon Bites Hard especially on Squirrels.")
+                    .SetCost(0, 0, 0, null)
+                    .AddAbilities(Ability.AllStrike, Ability.SquirrelOrbit, Ability.MadeOfStone, Ability.Reach)
+                    .AddTraits(Trait.Giant)
+                    .AddSpecialAbilities(SpecialTriggeredAbility.GiantCard)
+                    .AddAppearances(CardAppearanceBehaviour.Appearance.GiantAnimatedPortrait);
+                    Megaladon.animatedPortrait = GiantCardManager.Plugin.CreateGiantCard(TextureHelper.GetImageAsTexture("MEGALADON.png"));
+                    CardInfo MegaladonShip = CardManager.BaseGameCards.CardByName("!GIANTCARD_SHIP")
+                    .SetBasic(displayedName: "MEGALADON", attack: 1, health: 200, description: "Megaladon Bites Hard especially on Squirrels.")
+                    .SetCost(0, 0, 0, null)
+                    .AddAbilities(Ability.AllStrike, Ability.SquirrelOrbit, Ability.MadeOfStone, Ability.Reach)
+                    .AddTraits(Trait.Giant)
+                    .AddSpecialAbilities(SpecialTriggeredAbility.GiantCard)
+                    .AddAppearances(CardAppearanceBehaviour.Appearance.GiantAnimatedPortrait);
+                    MegaladonShip.animatedPortrait = GiantCardManager.Plugin.CreateGiantCard(TextureHelper.GetImageAsTexture("MEGALADON.png"));
+                }
+                else if (ReplaceMoon.BoxedValue.Equals("BossCardsLoader_MEGALADON"))
+                {
+                    CardInfo Megaladon = CardManager.BaseGameCards.CardByName("!GIANTCARD_MOON")
+                        .SetBasic(displayedName: "MEGALADON", attack: 2, health: 40, description: "Megaladon Bites Hard especially on Squirrels.")
+                        .SetCost(0, 0, 0, null)
+                        .AddAbilities(Ability.AllStrike, Ability.SquirrelOrbit, Ability.MadeOfStone, Ability.Reach)
+                        .AddTraits(Trait.Giant)
+                        .AddSpecialAbilities(SpecialTriggeredAbility.GiantCard)
+                        .AddAppearances(CardAppearanceBehaviour.Appearance.GiantAnimatedPortrait);
+                    Megaladon.animatedPortrait = GiantCardManager.Plugin.CreateGiantCard(TextureHelper.GetImageAsTexture("MEGALADON.png"));
+                }
+                else if (ReplaceLemoncello.BoxedValue.Equals("BossCardsLoader_MEGALADON"))
+                {
+                    CardInfo Megaladon = CardManager.BaseGameCards.CardByName("!GIANTCARD_SHIP")
+                         .SetBasic(displayedName: "MEGALADON", attack: 1, health: 200, description: "Megaladon Bites Hard especially on Squirrels.")
+                        .SetCost(0, 0, 0, null)
+                        .AddAbilities(Ability.AllStrike, Ability.SquirrelOrbit, Ability.MadeOfStone, Ability.Reach)
+                        .AddTraits(Trait.Giant)
+                        .AddSpecialAbilities(SpecialTriggeredAbility.GiantCard)
+                        .AddAppearances(CardAppearanceBehaviour.Appearance.GiantAnimatedPortrait);
+                    Megaladon.animatedPortrait = GiantCardManager.Plugin.CreateGiantCard(TextureHelper.GetImageAsTexture("MEGALADON.png"));
+                }
+                else
+                {
+                    CardInfo Megaladon = CardManager.New(modPrefix: "BossCardsLoader","!GIANTCARD_MEGALADON","MEGALADON",1,200,description: "Megaladon Bites Hard especially on Squirrels.")
+                    .SetCost(0, 0, 0, null)
+                    .AddAbilities(Ability.AllStrike, Ability.SquirrelOrbit, Ability.RandomAbility, Ability.Reach)
+                    .AddTraits(Trait.Giant)
+                    .AddSpecialAbilities(SpecialTriggeredAbility.GiantCard)
+                    .AddAppearances(CardAppearanceBehaviour.Appearance.GiantAnimatedPortrait);
                 CardManager.Add("BossCardsLoader", Megaladon);
-
-
                 Megaladon.animatedPortrait = GiantCardManager.Plugin.CreateGiantCard(TextureHelper.GetImageAsTexture("MEGALADON.png"));
-
+                }
             }
             i++;
             static void AddSUM()
             {
-                CardInfo SUM = CardManager.New(
-
-                    // Card ID Prefix
-                    modPrefix: "BossCardsLoader",
-
-                    // Card internal name.
-                    "!GIANTCARD_SUM",
-                    // Card display name.
-                    "SUM",
-                    // Attack.
-                    1,
-                    // Health.
-                    200,
-                    // Descryption.
-                    description: "Megaladon Bites Hard especially on Squirrels"
-            )
-                .SetCost(0, 0, 0, null)
-                .AddAbilities(Ability.AllStrike, Ability.SquirrelOrbit, Ability.RandomAbility, Ability.Reach)
-                .AddTraits(Trait.Giant)
-                .AddSpecialAbilities(SpecialTriggeredAbility.GiantCard)
-                .AddAppearances(CardAppearanceBehaviour.Appearance.GiantAnimatedPortrait)
-                ;
-                CardManager.Add("BossCardsLoader", SUM);
-
-
-                SUM.animatedPortrait = GiantCardManager.Plugin.CreateGiantCard(TextureHelper.GetImageAsTexture("SUM.png"));
+                if (ReplaceMoon.BoxedValue.Equals("BossCardsLoader_SUM") && ReplaceLemoncello.BoxedValue.Equals("BossCardsLoader_SUM"))
+                {
+                    CardInfo SUM = CardManager.BaseGameCards.CardByName("!GIANTCARD_MOON")
+                        .SetBasic(displayedName: "Сумеречная", attack: 2, health: 40, description: "Megaladon Bites Hard especially on Squirrels.")
+                        .SetCost(0, 0, 0, null)
+                        .AddAbilities(Ability.AllStrike, Ability.SquirrelOrbit, Ability.MadeOfStone, Ability.Reach)
+                        .AddTraits(Trait.Giant)
+                        .AddSpecialAbilities(SpecialTriggeredAbility.GiantCard)
+                        .AddAppearances(CardAppearanceBehaviour.Appearance.GiantAnimatedPortrait);
+                    SUM.animatedPortrait = GiantCardManager.Plugin.CreateGiantCard(TextureHelper.GetImageAsTexture("SUM.png"));
+                    CardInfo SUMShip = CardManager.BaseGameCards.CardByName("!GIANTCARD_SHIP")
+                        .SetBasic(displayedName: "Сумеречная", attack: 2, health: 40, description: "Megaladon Bites Hard especially on Squirrels.")
+                        .SetCost(0, 0, 0, null)
+                        .AddAbilities(Ability.AllStrike, Ability.SquirrelOrbit, Ability.MadeOfStone, Ability.Reach)
+                        .AddTraits(Trait.Giant)
+                        .AddSpecialAbilities(SpecialTriggeredAbility.GiantCard)
+                        .AddAppearances(CardAppearanceBehaviour.Appearance.GiantAnimatedPortrait);
+                    SUMShip.animatedPortrait = GiantCardManager.Plugin.CreateGiantCard(TextureHelper.GetImageAsTexture("SUM.png"));
+                }
+                else if (ReplaceMoon.BoxedValue.Equals("BossCardsLoader_SUM"))
+                {
+                    CardInfo SUM = CardManager.BaseGameCards.CardByName("!GIANTCARD_MOON")
+                        .SetBasic(displayedName: "Сумеречная", attack: 2, health: 40, description: "Megaladon Bites Hard especially on Squirrels.")
+                        .SetCost(0, 0, 0, null)
+                        .AddAbilities(Ability.AllStrike, Ability.SquirrelOrbit, Ability.MadeOfStone, Ability.Reach)
+                        .AddTraits(Trait.Giant)
+                        .AddSpecialAbilities(SpecialTriggeredAbility.GiantCard)
+                        .AddAppearances(CardAppearanceBehaviour.Appearance.GiantAnimatedPortrait);
+                    SUM.animatedPortrait = GiantCardManager.Plugin.CreateGiantCard(TextureHelper.GetImageAsTexture("SUM.png"));
+                }
+                else if (ReplaceLemoncello.BoxedValue.Equals("BossCardsLoader_SUM"))
+                {
+                    CardInfo SUM = CardManager.BaseGameCards.CardByName("!GIANTCARD_SHIP")
+                        .SetBasic(displayedName: "Сумеречная", attack: 2, health: 40, description: "Megaladon Bites Hard especially on Squirrels.")
+                        .SetCost(0, 0, 0, null)
+                        .AddAbilities(Ability.AllStrike, Ability.SquirrelOrbit, Ability.MadeOfStone, Ability.Reach)
+                        .AddTraits(Trait.Giant)
+                        .AddSpecialAbilities(SpecialTriggeredAbility.GiantCard)
+                        .AddAppearances(CardAppearanceBehaviour.Appearance.GiantAnimatedPortrait);
+                    SUM.animatedPortrait = GiantCardManager.Plugin.CreateGiantCard(TextureHelper.GetImageAsTexture("SUM.png"));
+                }
+                else
+                {
+                    CardInfo SUM = CardManager.New(modPrefix: "BossCardsLoader","!GIANTCARD_SUM","Сумеречная",2,40,description: "Megaladon Bites Hard especially on Squirrels")
+                        .SetCost(0, 0, 0, null)
+                        .AddAbilities(Ability.AllStrike, Ability.SquirrelOrbit, Ability.MadeOfStone, Ability.Reach)
+                        .AddTraits(Trait.Giant)
+                        .AddSpecialAbilities(SpecialTriggeredAbility.GiantCard)
+                        .AddAppearances(CardAppearanceBehaviour.Appearance.GiantAnimatedPortrait);
+                    CardManager.Add("BossCardsLoader", SUM);
+                    SUM.animatedPortrait = GiantCardManager.Plugin.CreateGiantCard(TextureHelper.GetImageAsTexture("SUM.png"));
+                }
             }
             i++;
             Logger.LogInfo($"Loaded {i} Boss Cards!");
